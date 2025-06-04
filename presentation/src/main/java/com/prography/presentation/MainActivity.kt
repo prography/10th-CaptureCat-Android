a221e7ad1b2b1172cb977094846bc39ed94eff67
wnsgml517 <wnsgml517@naver.com>
2025-06-05 00:32:15 +0900

    [feat/#2] edgeToedge로 인한 상/하단 상태바 패딩 값 주기

--- presentation/src/main/java/com/prography/presentation/MainActivity.kt
+++ presentation/src/main/java/com/prography/presentation/MainActivity.kt
@@ -8,7 +8,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.prography.ui.common.GlobalUiHandler
-import android.os.Handler
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.prography.navigation.NavigationHelper
 import javax.inject.Inject
@@ -19,25 +26,32 @@ class MainActivity : ComponentActivity() {
 
     @Inject
     lateinit var navigationHelper: NavigationHelper
-
     var isReady = false
+
+    private val viewModel: MainViewModel by viewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // 스플래시 화면 설치 (setContent 전에 호출해야 함)
         val splashScreen = installSplashScreen()
-        
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         // 2초 후에 스플래시 화면 종료
         splashScreen.setKeepOnScreenCondition { !isReady }
-
         isReady = true
         
         setContent {
             PrographyTheme {
-                AppNavGraph(navigationHelper = navigationHelper)
-                GlobalUiHandler()
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(WindowInsets.systemBars.asPaddingValues()) // 상단 + 하단 모두 자동 적용
+                ) {
+                    AppNavGraph(navigationHelper = navigationHelper)
+                    GlobalUiHandler()
+                }
             }
         }
     }
