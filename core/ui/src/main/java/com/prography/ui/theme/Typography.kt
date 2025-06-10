5a2412cf036882fbc006a5e2c8779439eb636c54
wnsgml517 <wnsgml517@naver.com>
2025-06-10 18:27:23 +0900

    [task/#1] 공통 폰트 설정

--- core/ui/src/main/java/com/prography/ui/theme/Typography.kt
+++ core/ui/src/main/java/com/prography/ui/theme/Typography.kt
@@ -6,27 +6,40 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.prography.ui.R
 
-val Pretendard = FontFamily(
-    Font(R.font.pretendard_regular),
-    Font(R.font.pretendard_medium),
-    Font(R.font.pretendard_semibold),
-    Font(R.font.pretendard_bold)
+val myFontFamily = FontFamily(
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold),
+    Font(R.font.pretendard_bold, FontWeight.Bold)
 )
 
 val AppTypography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = Pretendard,
-        fontSize = 16.sp
+    displayLarge = TextStyle(
+        fontFamily = myFontFamily,
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 40.sp
     ),
     titleLarge = TextStyle(
-        fontFamily = Pretendard,
-        fontSize = 22.sp
+        fontFamily = myFontFamily,
+        fontSize = 22.sp,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 28.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = myFontFamily,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 24.sp
     ),
     labelMedium = TextStyle(
-        fontFamily = Pretendard,
-        fontSize = 12.sp
+        fontFamily = myFontFamily,
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 16.sp
     )
 )
