344364b786f480e69563c3ce2fc28d1f5c4f7adf
wnsgml517 <wnsgml517@naver.com>
2025-07-22 11:04:25 +0900

    [fix/#21] 시작화면 분기처리

--- presentation/src/main/java/com/prography/presentation/MainViewModel.kt
+++ presentation/src/main/java/com/prography/presentation/MainViewModel.kt
@@ -2,14 +2,12 @@ package com.prography.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.prography.domain.repository.UserPreferenceRepository
-import com.prography.domain.usecase.auth.GetAuthTokenUseCase
+import com.prography.domain.usecase.auth.CheckLoginStatusUseCase
 import com.prography.domain.usecase.screenshot.GetAllScreenshotsUseCase
 import com.prography.domain.usecase.user.GetOnboardingShownUseCase
-import com.prography.domain.usecase.user.SetOnboardingShownUseCase
+import com.prography.domain.usecase.user.GetStartTagScreenShownUseCase
 import com.prography.navigation.AppRoute
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.first
@@ -17,11 +15,12 @@ import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
+
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val getOnboardingShownUseCase: GetOnboardingShownUseCase,
-    private val screenshotsUseCase: GetAllScreenshotsUseCase,
-    private val getAuthTokenUseCase: GetAuthTokenUseCase,
+    private val checkLoginStatusUseCase: CheckLoginStatusUseCase,
+    private val getStartTagScreenShownUseCase: GetStartTagScreenShownUseCase,
     private val authRepository: com.prography.domain.repository.AuthRepository
 ) : ViewModel() {
 
@@ -49,15 +48,15 @@ class MainViewModel @Inject constructor(
 
     fun initChecking() {
         viewModelScope.launch {
-            val isOnboardingShown = getOnboardingShownUseCase.invoke().first()
-            val isLoggedIn = getAuthTokenUseCase.isLoggedIn()
-            val isGetScreenshot = screenshotsUseCase.invoke().first().isNotEmpty()
+            val isOnboardingShown = getOnboardingShownUseCase().first()
+            val isLoggedIn = checkLoginStatusUseCase()
+            val isStartTagScreenShown = getStartTagScreenShownUseCase().first()
 
             _startDestination.value  = when {
-                !isOnboardingShown -> AppRoute.InitOnboarding // 처음 앱에 진입하는 경우, 초기 온보딩
-                !isGetScreenshot -> AppRoute.Start // 스크린샷이 없는 경우, 시작하기 화면
-                !isLoggedIn -> AppRoute.Login // 토큰이 존재하지 않는 경우, 로그인 화면
-                else -> AppRoute.Main // 토큰이 존재하는 경우, 메인 화면 : 유효 검사 interceptor 추후 처리
+                !isOnboardingShown -> AppRoute.InitOnboarding // 온보딩을 보지 않은 경우, 초기 온보딩 화면
+                isLoggedIn -> AppRoute.Main // 로그인 유저일 경우, 메인 화면
+                !isStartTagScreenShown -> AppRoute.Login // 게스트 모드일 경우, 시작하기를 안봤다면 로그인 화면
+                else -> AppRoute.Main  // 시작하기를 봤다면 메인 화면
             }
 
             isReady.value = true
